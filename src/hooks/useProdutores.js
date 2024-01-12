efc327e7f8b025d76d9e38cf658de208b5b8479c
GabrielBLucas11 <gabrielbl79@gmail.com>
2024-01-12 12:21:37 -0400

    Sorting produtor by star

--- src/hooks/useProdutores.js
+++ src/hooks/useProdutores.js
@@ -8,6 +8,9 @@ export default function useProdutores() {
 
   useEffect(() => {
     const retorno = carregaProdutores();
+    retorno.lista.sort(
+      (produtor1, produtor2) => produtor1.estrelas - produtor2.estrelas,
+    );
     setTitulo(retorno.titulo);
     setLista(retorno.lista);
   }, []);
